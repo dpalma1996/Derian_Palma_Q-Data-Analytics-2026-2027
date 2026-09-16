b628371f40fd55e1bf396c6e6e8f29ec3197e7ab
Derian Palma Quirós <70878553+dpalma1996@users.noreply.github.com>
2026-09-16 12:43:10 +0200

    Add files via upload

--- Sprint_04/Tasca S4.01._Derian_Palma_Q_NIVEL 3.sql
+++ Sprint_04/Tasca S4.01._Derian_Palma_Q_NIVEL 3.sql
@@ -78,9 +78,10 @@ SELECT
     product_id AS product_sku,
     p.name AS product_name,
     p.price AS product_price,
-    `sprint3-analytics-derian-palma.sprint3_gold.calculate_tax`(p.price)
-        AS product_price_tax_inc
-FROM `sprint3-analytics-derian-palma.sprint3_silver.transactions_clean` AS t
+    `sprint3-analytics-derian-palma.sprint3_gold.calculate_tax`(
+        p.price
+    ) AS product_price_tax_inc
+FROM `sprint3-analytics-derian-palma.sprint3_gold.fact_transactions_optimized` AS t
 CROSS JOIN UNNEST(t.product_ids) AS product_id
 JOIN `sprint3-analytics-derian-palma.sprint3_silver.products_clean` AS p
     ON product_id = p.product_id
